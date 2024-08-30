041fd13638a7b7e8ebffe74a29aa66c3a13aeb63
Yakovlev Denis <yakovlevdk39@gmail.com>
2024-08-30 16:20:43 +0300

    FIRST

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+// import styled from "styled-components";
+
+const App = function App() {
+  return (
+    <>
+      <i className="fa fa-camera-retro"></i>
+    </>
+  );
+};
+
+export default App;
